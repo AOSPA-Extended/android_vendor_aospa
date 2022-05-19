f2429581e1d9cb9c233f47f565908aa63086ed25
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-05-19 15:31:10 +0530

    overlay: Add Google Sans / Lato font
    Change-Id: Iaffe92b708af5e364ee446c7477e56f70bc67395

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     FontInterOverlay \
     FontManropeOverlay \
     FontUrbanistOverlay \
+    FontGoogleSansLatoOverlay \
     FontInterOverlay \
     FontManropeOverlay \
     FontRobotoOverlay \
